86ca36eda8a2a2fcb312944097c9fb97ff639ef6
NatalyaAlansi <alansinatalya@gmail.com>
2022-10-02 22:14:19 +0400

    homework 7: added the second function for datepicker

--- demoqa_tests/model/controls/datepicker.py
+++ demoqa_tests/model/controls/datepicker.py
@@ -1,5 +1,4 @@
 import datetime
-
 import selene
 from selene.support.shared import browser
 import demoqa_tests
@@ -10,3 +9,10 @@ def set_date(element: selene.Element, date: datetime.date):
         Keys.CONTROL + 'a' + Keys.NULL,
         date.strftime(demoqa_tests.config.datetime_format),
     ).press_enter()
+
+def set_date2():
+    browser.element('[id="dateOfBirthInput"]').click()
+    browser.element('.react-datepicker__year-select').element('[value="1993"]').click()
+    browser.element('.react-datepicker__month-select').element('[value="7"]').click()
+    browser.element('.react-datepicker__day--024').click()
+    return set_date2()
